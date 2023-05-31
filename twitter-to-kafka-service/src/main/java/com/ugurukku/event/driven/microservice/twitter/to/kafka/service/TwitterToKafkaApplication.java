accc0ca1dcc9c0c35c9325f5285243ed4de03868
ugurukku <ugurkarimli1@gmail.com>
2023-05-31 23:29:50 +0400

    initial commit

--- twitter-to-kafka-service/src/main/java/com/ugurukku/event/driven/microservice/twitter/to/kafka/service/TwitterToKafkaApplication.java
+++ twitter-to-kafka-service/src/main/java/com/ugurukku/event/driven/microservice/twitter/to/kafka/service/TwitterToKafkaApplication.java
@@ -1,2 +1,32 @@
-package com.ugurukku.event.driven.microservice.twitter.to.kafka.service;public class TwitterToKafkaApplication {
+package com.ugurukku.event.driven.microservice.twitter.to.kafka.service;
+
+import com.ugurukku.event.driven.microservice.twitter.to.kafka.service.config.ConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TwitterToKafkaApplication implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterToKafkaApplication.class);
+
+    private final ConfigData configData;
+
+    public TwitterToKafkaApplication(ConfigData configData) {
+        this.configData = configData;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaApplication.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("App starts..");
+        LOGGER.info(configData.getTwitterKeywords().toString());
+    }
 }
--- twitter-to-kafka-service/src/test/java/com/ugurukku/event/driven/microservice/twitter/to/kafka/service/TwitterToKafkaApplicationTest.java
+++ twitter-to-kafka-service/src/test/java/com/ugurukku/event/driven/microservice/twitter/to/kafka/service/TwitterToKafkaApplicationTest.java
@@ -1,4 +1,16 @@
+package com.ugurukku.event.driven.microservice.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class TwitterToKafkaApplicationTest {
-  
+
+    @Test
+    public void contextLoad(){
+
+    }
+
 }
