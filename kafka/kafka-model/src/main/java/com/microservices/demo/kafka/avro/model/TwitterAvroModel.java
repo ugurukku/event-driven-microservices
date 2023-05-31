f69ccc7228be5f23eafa2091456c02c8f493ce83
ugurukku <ugurkarimli1@gmail.com>
2023-06-01 03:05:54 +0400

    Avro model

--- kafka/kafka-model/src/main/java/com/microservices/demo/kafka/avro/model/TwitterAvroModel.java
+++ kafka/kafka-model/src/main/java/com/microservices/demo/kafka/avro/model/TwitterAvroModel.java
@@ -17,7 +17,7 @@ public class TwitterAvroModel extends org.apache.avro.specific.SpecificRecordBas
   private static final long serialVersionUID = -5190598237215532904L;
   public static final org.apache.avro.Schema SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"record\",\"name\":\"TwitterAvroModel\",\"namespace\":\"com.microservices.demo.kafka.avro.model\",\"fields\":[{\"name\":\"userId\",\"type\":\"long\"},{\"name\":\"id\",\"type\":\"long\"},{\"name\":\"text\",\"type\":[\"null\",{\"type\":\"string\",\"avro.java.string\":\"String\"}]},{\"name\":\"createdAt\",\"type\":[\"null\",\"long\"],\"logicalType\":[\"null\",\"date\"]}]}");
   public static org.apache.avro.Schema getClassSchema() { return SCHEMA$; }
-//test
+//test test2
   private static SpecificData MODEL$ = new SpecificData();
 
   private static final BinaryMessageEncoder<TwitterAvroModel> ENCODER =
