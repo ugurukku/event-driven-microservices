487197d0a23f6d4ef72136d99f27bed15e2d1060
ugurukku <ugurkarimli1@gmail.com>
2023-06-03 02:12:34 +0400

    initializing kafka producer

--- twitter-to-kafka-service/src/test/java/com/ugurukku/event/driven/microservice/twitter/to/kafka/service/TwitterToKafkaApplicationTest.java
+++ twitter-to-kafka-service/src/test/java/com/ugurukku/event/driven/microservice/twitter/to/kafka/service/TwitterToKafkaApplicationTest.java
@@ -1,16 +1,16 @@
-package com.ugurukku.event.driven.microservice.twitter.to.kafka.service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class TwitterToKafkaApplicationTest {
-
-    @Test
-    public void contextLoad(){
-
-    }
-
-}
+//package com.ugurukku.event.driven.microservice.twitter.to.kafka.service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class TwitterToKafkaApplicationTest {
+//
+//    @Test
+//    public void contextLoad(){
+//
+//    }
+//
+//}
